c4b38ca5da58ca9b62a135d9fb8100759d096b79
asiapanizza <asia.panizza@gmail.com>
2025-12-01 16:13:23 +0100

    tried to implement genetic programming, fixing is still needed

--- experiment.py
+++ experiment.py
@@ -1,10 +1,13 @@
 
-from deap import gp, base, creator, tools
+from deap import gp, base, creator, tools, algorithms
 import operator
 import math
 import random
 from ACO.ACO_simulator import ACO_simulator
 from sanity_check import weight_func, heuristic
+from meshgraph import MeshGraph
+import numpy as np
+import functools
 
 
 # FITNESS AND INDIVIDUAL SET UP
@@ -21,7 +24,7 @@ def protectedDiv(num, den):
     else:
         return num/den
 
-    
+# NB: evaluate whether to include "water" and "building" as arguments    
 pset = gp.PrimitiveSet("main_elements", 4)
 pset.renameArguments(ARG0 = "dist", ARG1 = "elev_diff", ARG2 = "elev_u", ARG3 = "elev_v")
 pset.addPrimitive(operator.add, 2, "add")
@@ -32,12 +35,14 @@ pset.addPrimitive(operator.neg, 1, "neg")
 pset.addPrimitive(math.log, 1, name ="log")
 pset.addPrimitive(operator.pow, 2, "power")
 
+
+
 #adding use of coefficients between -5 and 5, can change according to performance
-pset.addEphemeralConstant("coefficients", lambda:random.uniform(-5.0, 5.0))
+pset.addEphemeralConstant("coefficients", functools.partial(random.uniform,-5.0, 5.0))
 
 # BOOTSTRAPPING INITIALIZATION
 
-toolbox = base.Toolbox
+toolbox = base.Toolbox()
 
 # function to generate trees using the primitive set I set before
 # choosing genHalfAndHalf (ramped half-and-half method) for balance between exploration (genFull) and exploitation (genGrow)
@@ -45,42 +50,92 @@ toolbox = base.Toolbox
 
 toolbox.register("gen", gp.genHalfAndHalf, pset = pset, min_ = 1, max_ = 5)
 
-#tools.initIterate invokes the "gen" function which creates a tree and then stores the tree in the individual container
+#tools.initIterate invokes the "gen" function which createsn a tree and then stores the tree in the individual container
 toolbox.register("individual", tools.initIterate, creator.Individual, toolbox.gen)
 
 # repeats the "individual" function for N times and stores the trees in a list
-toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+toolbox.register("population", tools.initRepeat, container = list, func = toolbox.individual)
 
-
-# NB. NOTES SAY THAT CROSSOVER IS SET AS HIGHLY PROBABLE (P>0.8) AND MUTATION AS QUITE RARE (P < 0.05)
-
+#names must be fixed or it doesn't work
 #crossover #swaps two individuals in correspondence of random nodes (swaps from node onward)
-toolbox.register("crossover", gp.cxOnePoint)
+toolbox.register("mate", gp.cxOnePoint)
 #mutation #from a random node within tree the tree-generation function is called and mutated tree can even end up exceeding max depth
-toolbox.register("mutation", gp.mutUniform, expr = toolbox.gen, pset = pset)
+toolbox.register("mutate", gp.mutUniform, expr = toolbox.gen, pset = pset)
 #selection #performs tournaments N tournaments where random 3 trees are sampled and best one survives
-toolbox.register("selection", tools.selTournament, tournsize = 3)
+toolbox.register("select", tools.selTournament, tournsize = 3)
 
 #it turns deap.function into a python function
 toolbox.register("compile", gp.compile, pset = pset)
 
-def evaluate_aco_fitness(individual, graph, aco_params):
-    try:
-        cost_func = toolbox.compile(expr = individual)
-    except:
-        return float("inf") #worst fitness possible
-    def updated_weight_func(graph, u,v):
-        if graph.nodes[u].get('is_water') or graph.nodes[v].get('is_water'):
-            return float('inf') # Impossible to cross water
-        dist = heuristic(u,v)
-        elev_v = graph.nodes[v]["elevation"]
-        elev_u = graph.nodes[u]["elevation"]
-        elev_diff = abs(elev_u - elev_v)
-        # cost value computed with values of arch and specific individual tree
-        cost = cost_func(dist, elev_diff,elev_u, elev_v)
-        return max(cost, 1.0) #cannot return negative value, ACO could fail
+G = MeshGraph()
+
+ant_colony_parameters = {"alpha": 1, "beta": 2, "rho": 0.1, "ant_number": 5, "max_iterations": 10, "max_no_updates": 50, "n_best_ants": 5, "average_cycle_length": 3600}
+
+
+
+
+class WeightFunction:
+    def __init__(self, cost_func, graph, heuristic_func):
+        self.cost_func = cost_func
+        self.graph = graph
+        self.heuristic_func = heuristic_func
     
-    graph.weight_func = updated_weight_func
+    def __call__(self, u, v):
+        if self.graph.nodes[u].get('is_water') or self.graph.nodes[v].get('is_water'):
+            return float('inf')
+        
+        dist = self.heuristic_func(u, v)
+        elev_v = self.graph.nodes[v]["elevation"]
+        elev_u = self.graph.nodes[u]["elevation"]
+        elev_diff = abs(elev_u - elev_v)
+        cost = self.cost_func(dist, elev_diff, elev_u, elev_v)
+        return max(cost, 1.0)
+
+def evaluate_aco_fitness(individual, graph, aco_params):
+    og_weight_func = getattr(graph, 'weight_func', None)
+    try:
+        cost_func = toolbox.compile(expr=individual)
+    except Exception:
+        return (float('inf'),)  # Return tuple with inf instead of arbitrary number
+    
+    # Create picklable weight function
+    graph.weight_func = WeightFunction(cost_func, graph, heuristic)
+    
+    path_cost = float("inf")
+    try:
+        aco = ACO_simulator(graph, **aco_params)
+        best_paths = aco.simulation(retrieve_n_best_paths=1)
+        if best_paths:
+            path_cost = best_paths[0][1]
+    except Exception as e:
+        print(f"ACO simulation failed: {e}")
+        pass
+    finally:
+        if og_weight_func is not None:
+            graph.weight_func = og_weight_func
+        else:
+            delattr(graph, 'weight_func')
+    
+    return (path_cost,)
+
+
+toolbox.register("evaluate", functools.partial(evaluate_aco_fitness, graph = G, aco_params = ant_colony_parameters))
+
+def main():
+    random.seed(31)
+    try:
+        pop = toolbox.population(n = 200)
+    except:
+        return 10101010
+    hof = tools.HallOfFame(1)
+    stats = tools.Statistics(lambda ind: ind.fitness.values)
+    stats.register("min", np.min)
+    pop,log = algorithms.eaSimple(pop, toolbox, cxpb =0.8, mutpb = 0.05, ngen = 20, stats = stats, halloffame=hof, verbose = True) 
+    return pop, log, hof
+
+if __name__ == "__main__":
+    main()
+
     
 
 
