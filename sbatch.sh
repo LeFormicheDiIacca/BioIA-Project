34edf0a91ed89faa5e53cc4f7667c0ba298009f2
marcoff181 <marcoff181@users.noreply.github.com>
2026-01-08 00:52:13 +0100

    actual partition

--- sbatch.sh
+++ sbatch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH -p short
+#SBATCH -p edu-medium
 #SBATCH -n 1
 #SBATCH -c 8
 #SBATCH -N 1
-#SBATCH -t 0-00:15
+#SBATCH -t 0-01:15
 #SBATCH --gres=gpu:0
 
 source /home/${USER}/.bashrc
