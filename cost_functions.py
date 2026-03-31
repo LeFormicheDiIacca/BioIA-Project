2c58084d09777cddbe68d3852ec6d8fe670f8e9d
davide <davicolosimo23@gmail.com>
2026-03-31 14:04:47 +0200

    New evolved cost functions

--- cost_functions.py
+++ cost_functions.py
@@ -1,5 +1,9 @@
 import math
 
+import numpy as np
+
+from GP.gp_logistics import protected_pow, protected_log, protected_div
+
 
 # NB. all the parameters I used to evaluate the function can be found in the edge_info.py file,
 # please choose whether YOU 
@@ -9,58 +13,44 @@ import math
 
 #   OR if you want to implement get_edge_metadata edge by edge
 
-def best_CF(distance, steepness, elev_u, elev_v, is_water):
-    steepness = 2*steepness
-    if is_water == 1.0:
-        ret = math.log(3.148, 9.535)
-    else:
-        ret = distance + distance
-    tmp = ((distance + 3.685) + (elev_u * elev_u / 4.164)) - elev_v
-    if is_water == 1.0:
-        tmp -= steepness
-    else:
-        tmp -= elev_v
-    ret -= tmp
-    ret -= (2.41 ** 3.978)
-    ret = (-1) * ret
-    return ret
+#Trentino
+def trentino_first_cost_function(distance, steepness, is_water):
+    term1 = protected_pow(distance,0.592)
+    term_2 = np.where(is_water,protected_pow(distance,steepness-0.827), steepness)
+    term_2 += 1.378+distance
+    return term1 + term_2
 
+def trentino_second_cost_function(distance, steepness, is_water):
+    tmp_1= np.where(is_water, protected_log(distance, 0.628), steepness)
+    temp2 = 1.837 + distance + protected_pow(distance,0.628)
+    return tmp_1 + temp2
 
-def second_best_CF(distance, steepness, elev_u, elev_v, is_water):
-    steepness = 2*steepness
-    ret = elev_u * steepness * distance
-    if is_water == 1.0:
-        ret += distance
-    else:
-        ret += elev_u * elev_v * steepness
-    return ret
+def trentino_third_cost_function(distance, steepness, is_water):
+    tmp_1 = np.where(is_water, 1/distance, steepness)
+    tmp_2 = 1.439++distance+protected_pow(distance,0.647)
+    return tmp_1 + tmp_2
 
+def trentino_fourth_cost_function(distance, steepness, is_water):
+    tmp_1 = np.where(is_water,0.884, steepness)
+    tmp_2 = protected_pow(distance,0.586)+1.380+distance
+    return tmp_1 + tmp_2
+#Naples
 
-def third_best_CF(distance, steepness, elev_u, elev_v, is_water):
-    steepness = 2*steepness
-    ret = elev_u * steepness * distance
-    if is_water == 1.0:
-        ret += steepness - elev_v
-    else:
-        ret += elev_u - elev_v
-    return ret
+def naples_first_cost_function(distance, steepness, is_water):
+    tmp_1 = np.where(is_water, 0.117, 0.11)
+    tmp_2 = 0.084 *(steepness+distance)
+    return  tmp_1 * tmp_2
 
+def naples_second_cost_function(distance, steepness, is_water):
+    tmp_1 = np.where(is_water, 0.517, steepness)
+    tmp_2 = distance+0.803+protected_div(distance+steepness,0.153)
+    return tmp_1 + tmp_2
 
-def fourth_best_CF(distance, steepness, elev_u, elev_v, is_water):
-    steepness = 2*steepness
-    ret = elev_u * steepness * distance
-    if is_water == 1.0:
-        ret += distance
-    else:
-        ret += elev_u - elev_v
-    return ret
+def naples_third_cost_function(distance, steepness, is_water):
+    tmp_1 =np.where(is_water, 0.215, steepness)
+    return protected_pow(tmp_1+0.786+distance, 0.786)
 
-
-def fifth_best_CF(distance, steepness, elev_u, elev_v, is_water):
-    steepness = 2*steepness
-    if is_water == 1.0:
-        tmp = distance
-    else:
-        tmp = steepness
-    ret = distance - elev_u + tmp + elev_v
-    return ret
+def naples_fourth_cost_function(distance, steepness, is_water):
+    #Not so good
+    tmp_1 = np.where(is_water, 0.629, steepness)
+    return 0.009* distance+tmp_1
