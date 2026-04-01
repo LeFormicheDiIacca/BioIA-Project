a4657f3309fe534ac18046fdf14708980b9295f9
davide <davicolosimo23@gmail.com>
2026-04-01 16:21:48 +0200

    Fixed aggregator

--- GP/evolved_cost_functions.py
+++ GP/evolved_cost_functions.py
@@ -1,61 +1,59 @@
 import numpy as np
 
-from GP.gp_logistics import protected_log, protected_pow
+from GP.gp_logistics import protected_log, protected_pow, protected_div
 
 #Trentino
-def first_cost_function(distance, steepness, is_water):
+def trentino_first_cost_function(distance, steepness, is_water):
+    #Run on Trento-Laives of 30/03/2026 3500pop_50gen_200res
+    #Final HoF, second individual. Fitness:86.9742238105947
     term1 = protected_pow(distance,0.592)
     term_2 = np.where(is_water,protected_pow(distance,steepness-0.827), steepness)
     term_2 += 1.378+distance
     return term1 + term_2
 
-def second_cost_function(distance, steepness, is_water):
+def trentino_second_cost_function(distance, steepness, is_water):
+    #Run on Trento-Laives of 30/03/2026 3500pop_50gen_200res_"
+    #Final HoF, fourth individual. Fitness:86.9742280926197
     tmp_1= np.where(is_water, protected_log(distance, 0.628), steepness)
     temp2 = 1.837 + distance + protected_pow(distance,0.628)
     return tmp_1 + temp2
 
-def third_cost_function(distance, steepness, is_water):
+def trentino_third_cost_function(distance, steepness, is_water):
     tmp_1 = np.where(is_water, 1/distance, steepness)
-    tmp_2 = 1.439++distance+protected_pow(distance,0.647)
+    tmp_2 = 1.439+distance+protected_pow(distance,0.647)
     return tmp_1 + tmp_2
 
-def fourth_cost_function(distance, steepness, is_water):
+def trentino_fourth_cost_function(distance, steepness, is_water):
     tmp_1 = np.where(is_water,0.884, steepness)
     tmp_2 = protected_pow(distance,0.586)+1.380+distance
     return tmp_1 + tmp_2
+#Naples
 
-def second_best_CF(distance, steepness, elev_u, elev_v, is_water):
-    ret = elev_u * steepness * distance
-    if is_water == 1.0:
-        ret += distance
-    else:
-        ret += elev_u*elev_v*steepness
-    return ret
+def naples_first_cost_function(distance, steepness, is_water):
+    #Run on Mondragone-Capua of 31/03/2026 3500pop_50gen_200res_3
+    #Final HoF, first individual. Fitness:97.73775153818191
+    tmp_1 = np.where(is_water, 0.117, 0.11)
+    tmp_2 = 0.084 *(steepness+distance)
+    return  tmp_1 * tmp_2
 
-def third_best_CF(distance, steepness, elev_u, elev_v, is_water):
-    ret = elev_u*steepness*distance
-    if is_water == 1.0:
-        ret += steepness - elev_v
-    else:
-        ret += elev_u - elev_v
-    return ret
+def naples_second_cost_function(distance, steepness, is_water):
+    #Run on Mondragone-Capua of 31/03/2026 3500pop_50gen_200res_4
+    #Final HoF, last individual. Fitness:97.73333262244897
+    tmp_1 = np.where(is_water, 0.517, steepness)
+    tmp_2 = distance+0.792+protected_div(distance+steepness,0.153)
+    return tmp_1 + tmp_2
 
-def fourth_best_CF(distance, steepness, elev_u, elev_v, is_water):
-    ret = elev_u * steepness * distance
-    if is_water == 1.0:
-        ret += distance
-    else:
-        ret += elev_u - elev_v
-    return ret
-    
+def naples_third_cost_function(distance, steepness, is_water):
+    #Run on Mondragone-Capua of 31/03/2026 3500pop_50gen_200res_5
+    #Final HoF, last individual. Fitness:97.73775153818191
+    tmp_1 =np.where(is_water, 0.215, steepness)
+    return protected_pow(tmp_1+0.786+distance, 0.786)
 
-def fifth_best_CF(distance, steepness, elev_u, elev_v, is_water):
-    if is_water == 1.0:
-        tmp = distance
-    else:
-        tmp = steepness
-    ret = distance - elev_u + tmp + elev_v
-    return ret
+def naples_fourth_cost_function(distance, steepness, is_water):
+    #Run on Mondragone-Capua of 31/03/2026 3500pop_50gen_200res_7
+    #Final HoF, second individual. Fitness:97.73775153818191
+    tmp_1 = np.where(is_water, 0.629, steepness)
+    return 0.009* distance+tmp_1
 
 
 if __name__ == "__main__":
--- Results/results_aggregator.py
+++ Results/results_aggregator.py
@@ -33,14 +33,14 @@ def extract_path_statistics(html_file_path):
 date = "17_01_2026"
 #Nodes Mapping to paths
 key_nodes = dict()
-key_nodes[(34321, 16744, 5971, 5628)] = "Pergine Valsugana, Trento\nSopramonte, Vason"
-key_nodes[(7028, 5678, 16099, 23538, 34371)] = "Trento, Lavis\nCembra, Capriana"
-key_nodes[(34295, 23771, 5628)] = "Trento, Sopramonte\nSarche"
-key_nodes[(5628, 14161, 24166, 34371)] = "Fondo\nPasso della Mendola\nAppiano, Laives"
-key_nodes[(5628, 15865, 21927, 34371)] = "Trento, Baselga di Piné\nValfloriana, Moena"
-key_nodes[(6428, 5688, 34371)] = "Trento, Mezzocorona\nLaives"
-key_nodes[(5628, 17371, 34309)] = "Mezzocorona, Cavalese\nCanal San Bovo"
-key_nodes[(34228, 24297, 17937, 5771)] = "Trento, Mezzolombardo\nDenno, Cles"
+key_nodes[(5628,16507,18371,7359,34253)] = "1-Path North Campania 1"
+key_nodes[(5710,9771,23932,34317,28282,25228)] = "2-Path North Campania 2"
+key_nodes[(15761,24571,29351,32299,34236,24056,12228,5678)] = "3-Path North Campania 3"
+key_nodes[(10028,9360,21771,34276,5679)] = "4-Path North Campania 4"
+key_nodes[(34321,22570,16744,12130,5971,8088,5628)] = "5-Path Trentino 1"
+key_nodes[(7028,5678,9489,16099,23538,26760,34371)] = "6-Path Trentino 2"
+key_nodes[(5628,8314,14161,20767,24166,34371)] = "7-Path Trentino 3"
+key_nodes[(5628, 10243, 15865, 21927, 24342, 34371)] = "8-Path Trentino 4"
 
 data_records = []
 
@@ -112,7 +112,10 @@ sns.set_theme(style="whitegrid")
 metrics = ["Average Cost", "2D Length", "3D Length", "Avg Inclination"]
 titles = ["Average Cost", "Average Planar Length (km)", "Average Real Length (km)", "Average Steepness (%)"]
 
-
+if not df.empty:
+    output_filename = f"path_data{date}.csv"
+    df.to_csv(output_filename, index=False, encoding='utf-8-sig', sep=';')
+"""
 for idx, metric in enumerate(metrics):
     fig, ax = plt.subplots(1, 1, figsize=(16, 12))
     sns.barplot(
@@ -131,4 +134,4 @@ for idx, metric in enumerate(metrics):
     ax.legend(title="Cost Function", loc='upper right')
     plt.tight_layout()
     plt.savefig(f"{titles[idx]}.svg", dpi=300)
-    plt.show()
+    plt.show()"""
